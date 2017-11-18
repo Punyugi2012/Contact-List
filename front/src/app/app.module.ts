82537b4920cca3549e73d052601b4e32071d4659
Punyugi2012 <58160216@go.buu.ac.th>
2017-11-18 19:18:36 +0700

    update

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { PersonProfileComponent } from './person-profile/person-profile.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+import { EditPersonComponent } from './edit-person/edit-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactListComponent,
+    PersonProfileComponent,
+    AddPersonComponent,
+    EditPersonComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

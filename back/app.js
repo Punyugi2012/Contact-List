631ed946effafe771fc0da6ae018ee6fd409d178
Punyugi2012 <58160216@go.buu.ac.th>
2017-11-22 20:34:12 +0700

    fix server and service

--- back/app.js
+++ back/app.js
@@ -7,12 +7,12 @@ var app = express();
 
 app.use(cors());
 app.use(function (req, res, next) {
-    console.log(`Request: ${req.url} ${new Date()}`);
+    console.log(`Request: ${req.method}.....${req.url}.....${new Date()}`);
     next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/person', personRouter);
+app.use('/api/persons', personRouter);
 app.use('*', function (req, res) {
     res.status(404);
     res.json({});
